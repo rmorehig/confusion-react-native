f397d4c57faa9215b81309ea287e829bdce41631
Rafael Moreno <rmh1516@gmail.com>
2019-09-22 14:25:16 +0200

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
-import { Card } from 'react-native-elements';
-import * as Animatable from 'react-native-animatable';
+import React, { Component } from "react";
+import { Text, StyleSheet } from "react-native";
+import { Card, Button, Icon } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
+import { MailComposer } from "expo";
 
 class Contact extends Component {
+  static navigationOptions = {
+    title: "Contact Us"
+  };
 
-    static navigationOptions = {
-        title: 'Contact Us'
-    };
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ["confusion@food.net"],
+      subject: "Enquiry",
+      body: "To whom it may concern:"
+    });
+  }
 
   render() {
     return (
-      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>                
-        <Card
-            title='Contact Information'>
-          <Text
-            style={styles.text}>
-            121, Clear Water Bay Road</Text>
-          <Text
-            style={styles.text}>
-            Clear Water Bay, Kowloon</Text>
-          <Text
-            style={styles.text}>
-            HONG KONG</Text>
-          <Text
-            style={styles.text}>
-            Tel: +852 1234 5678</Text>
-          <Text
-            style={styles.text}>
-            Fax: +852 8765 4321</Text>
-          <Text
-            style={styles.text}>
-            Email:confusion@food.net</Text>
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+        <Card title="Contact Information">
+          <Text style={styles.text}>121, Clear Water Bay Road</Text>
+          <Text style={styles.text}>Clear Water Bay, Kowloon</Text>
+          <Text style={styles.text}>HONG KONG</Text>
+          <Text style={styles.text}>Tel: +852 1234 5678</Text>
+          <Text style={styles.text}>Fax: +852 8765 4321</Text>
+          <Text style={styles.text}>Email:confusion@food.net</Text>
+          <Button
+            title="Send Email"
+            buttonStyle={{ backgroundColor: "#512DA8" }}
+            icon={<Icon name="envelope-o" type="font-awesome" color="white" />}
+            onPress={this.sendMail}
+          />
         </Card>
       </Animatable.View>
-    )
+    );
   }
-
 }
 
 const styles = StyleSheet.create({
